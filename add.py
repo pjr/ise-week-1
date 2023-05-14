87168e68ad04dfcf25969ae729758f033aa46350
Philip Reynolds <phil@growlabs.io>
2023-05-14 22:50:35 +0100

    Initial version of add.py

---
+++ add.py
@@ -0,0 +1,10 @@
+def add(x,  y):
+    return x + y
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, -1) == -2
+    assert add(1, -1) == 0
+
+if __name__ == "__main__":
+    print(add(3, 2))
