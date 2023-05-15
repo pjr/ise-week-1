bc97373191c411d1f8f4cbb57634fa51a68d74cf
Philip Reynolds <phil@growlabs.io>
2023-05-15 12:21:32 +0100

    Added subtract() function

--- add.py
+++ add.py
@@ -1,10 +1,18 @@
 def add(x,  y):
     return x + y
 
+def subtract(x, y):
+    return x + y
+
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, -1) == -2
     assert add(1, -1) == 0
 
+def test_subtract():
+    assert subtract(10, 3) == 7
+    assert subtract(3, 10) == -7
+
 if __name__ == "__main__":
     print(add(3, 12))
+    print(subtract(12,3))
